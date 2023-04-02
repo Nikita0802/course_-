cc66c8b7e7393ffee5a5cf53b7c3b76dd9fc6bab
Nikita Boldyrev <nikita08022000@mail.ru>
2023-04-02 21:41:45 +0300

    start

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { Htag } from '@/components';
+
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+<Htag tag={'h1'} >Текст 1</Htag>
+    </>
+  );
+}
