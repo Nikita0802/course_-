e20ef50f3cbc9f51bf7a37f8e1cfa33b37f92375
Nikita Boldyrev <nikita08022000@mail.ru>
2023-04-06 18:19:16 +0300

    Rating

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
+    </Html >
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import { Htag } from '@/components';
+import React, { useState } from 'react';
+import { Htag, Button, P, Tag, Rating } from '../components';
 
 
-const inter = Inter({ subsets: ['latin'] });
+
+
 
 export default function Home(): JSX.Element {
+
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
-<Htag tag={'h1'} >Текст 1</Htag>
+      <Htag tag={'h1'} >Текст 1</Htag>
+      <Button appearance='primary' arrow='right' >Кнопка 1</Button>
+      <Button appearance='ghost' arrow='down'>Кнопка 2</Button>
+      <P size='m'>
+        Идет два программиста по улице, один говорит другому:
+      </P>
+      <Tag size='m' >Ты знаешь, моя жена сказала, что если я еще раз проведу ночь с клавиатурой, то она уйдет от меня.</Tag>
+      <Tag size='s' color='red'> И как ты собираешься это предотвратить?</Tag>
+      <Tag color='primary' >Я перейду на мышь.</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
